50c05006ab08251a1c04e39e525b96248abaabad
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-08-27 19:45:36 +0530

    Updated the function for factorial

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,15 @@
-print("hello bolrd!")
+import os
+os.system("cls")
+
+def factorial(x):
+	if x == 0:
+		return 1
+	if x<0:
+		return -1
+	return x*factorial(x-1)
+
+    
+
+x = int(input("Enter the number whose factorial is to be found"))
+
+print(factorial(x))
