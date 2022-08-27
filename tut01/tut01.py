902019f72f14462ba7e489d659b0269842b40773
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-08-27 19:50:52 +0530

    Updated comments

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,15 +1,15 @@
 import os
 os.system("cls")
 
-def factorial(x):
-	if x == 0:
+def factorial(x): ## factorial function declared 
+	if x == 0:  ## when input is 0
 		return 1
-	if x<0:
+	if x<0: 	## when input is negative
 		return -1
-	return x*factorial(x-1)
+	return x*factorial(x-1) ## recurse
 
     
 
-x = int(input("Enter the number whose factorial is to be found"))
+x = int(input("Enter the number whose factorial is to be found:\n")) ## taking input
 
-print(factorial(x))
+print("The factorial of", x, "is:", factorial(x), ".") ## here is the output
