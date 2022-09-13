46980e2d76c6da7edd51f3caf711cda4af725272
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-09-13 22:56:31 +0530

    1st basic update
    the code to check python version itself was buggy. hence that edit. ok.

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,14 +1,19 @@
+import pandas as pd
+import csv
+import platform
 def octact_identification(mod=5000):
 ###Code
 
+## upd: updated, minor bugs and better functionality
 
-from platform import python_version
-ver = python_version()
 
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+    from platform import python_version
+    ver = python_version()
+
+    if ver >= "3.8.10":
+        print("Correct Version Installed")
+    else:
+        print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 mod=5000
 octact_identification(mod)
