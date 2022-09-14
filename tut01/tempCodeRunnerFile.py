8015805a0f50bdef9822beceb000481befd7d8ed
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-09-14 21:33:53 +0530

    added last parts after octant calculation and improved minor bugs

---
+++ tut01/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+    #     os.remove("octant_output.csv")
+    # df.to_csv('octant_output.csv')
--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,116 +1,111 @@
-import pandas as pd
-import pandas as pd
+# Writer:
+# Harsh Mangalam Verma (IIT Patna) 
+# For tut01
 
-# UPD: original code from tutorial that checked python version removed - not needed anymore
-df = pd.read_csv("octant_input.csv")
+def octant_identification(mod = 4000):
+    import pandas as pd
+    import math
+    import os
+    from pathlib import Path
 
-df.at[0, "U Avg"] = df["U"].mean()
-df.at[0, "V Avg"] = df["V"].mean()
-df.at[0, "W Avg"] = df["W"].mean()
-df.at[0, "U Avg"] = df["U"].mean()
-df = df.fillna()
-sz = len(df)
+    df = pd.read_csv("tut01\octant_input.csv")
 
-# this function calculates the differences required
-def fun(a, b, c, sz):
+    # creating and populating colums that store Avg values
+    df.at[0, "U Avg"] = df["U"].mean()
+    df.at[0, "V Avg"] = df["V"].mean()
+    df.at[0, "W Avg"] = df["W"].mean()
+    df.at[0, "U Avg"] = df["U"].mean()
+    sz = len(df)
+
+    # this function calculates the differences required
+    def fun(a, b, c, sz):
+        for i in range(sz):
+            df.at[i, c] = df.at[i, a] - df.at[0, b]
+
+    # function calls for creating new columns that store, while calculating as per requirements at the same time
+    fun("U", "U Avg", "U'", sz)
+    fun("V", "V Avg", "V'", sz)
+    fun("W", "W Avg", "W'", sz)
+    df = df.fillna('')
+    # df
+
+    # Populating the Octant column, that is created in first call itself, with corresponding row values of U' V' W' 
     for i in range(sz):
-        df.at[i, c] = df.at[i, a] - df.at[0, b]
+        if(df.at[i, "U'"] >= 0 and df.at[i, "V'"] >= 0 and df.at[i, "W'"] >= 0):
+            df.at[i, "Octant"] = 1
+        if(df.at[i, "U'"] >= 0 and df.at[i, "V'"] >= 0 and df.at[i, "W'"] < 0):
+            df.at[i, "Octant"] = -1
+        if(df.at[i, "U'"] < 0 and df.at[i, "V'"] >= 0 and df.at[i, "W'"] >= 0):
+            df.at[i, "Octant"] = 2
+        if(df.at[i, "U'"] < 0 and df.at[i, "V'"] >= 0 and df.at[i, "W'"] < 0):
+            df.at[i, "Octant"] = -2
+        if(df.at[i, "U'"] < 0 and df.at[i, "V'"] < 0 and df.at[i, "W'"] >= 0):
+            df.at[i, "Octant"] = 3
+        if(df.at[i, "U'"] < 0 and df.at[i, "V'"] < 0 and df.at[i, "W'"] < 0):
+            df.at[i, "Octant"] = -3
+        if(df.at[i, "U'"] >= 0 and df.at[i, "V'"] < 0 and df.at[i, "W'"] >= 0):
+            df.at[i, "Octant"] = 4
+        if(df.at[i, "U'"] >= 0 and df.at[i, "V'"] < 0 and df.at[i, "W'"] < 0):
+            df.at[i, "Octant"] = -4
 
-# df.drop("W'=W - W avg", inplace=True, axis=1)
+    # df
 
-# function calls for meking changes as pre requirement for the columns
-fun("U", "U Avg", "U'", sz)
-fun("V", "V Avg", "V'", sz)
-fun("W", "W Avg", "W'", sz)
+    df["User Activity"] = ""
+    df.at[2, "User Activity"] = "User Input->"
+    df["Octant ID"] = ''
+    df.at[1, "Octant ID"] = "Overall Count"
+    df.at[2, "Octant ID"] = f"mod: {mod}"
 
-# df
+    mn = 0
+    mx = mod - 1
+    rowMax = sz // mod
 
-for i in range(sz):
-    if(df.at[i, "U'"] >= 0 and df.at[i, "V'"] >= 0 and df.at[i, "W'"] >= 0):
-        df.at[i, "Octant"] = 1
-    if(df.at[i, "U'"] >= 0 and df.at[i, "V'"] >= 0 and df.at[i, "W'"] < 0):
-        df.at[i, "Octant"] = -1
-    if(df.at[i, "U'"] < 0 and df.at[i, "V'"] >= 0 and df.at[i, "W'"] >= 0):
-        df.at[i, "Octant"] = 2
-    if(df.at[i, "U'"] < 0 and df.at[i, "V'"] >= 0 and df.at[i, "W'"] < 0):
-        df.at[i, "Octant"] = -2
-    if(df.at[i, "U'"] < 0 and df.at[i, "V'"] < 0 and df.at[i, "W'"] >= 0):
-        df.at[i, "Octant"] = 3
-    if(df.at[i, "U'"] < 0 and df.at[i, "V'"] < 0 and df.at[i, "W'"] < 0):
-        df.at[i, "Octant"] = -3
-    if(df.at[i, "U'"] >= 0 and df.at[i, "V'"] < 0 and df.at[i, "W'"] >= 0):
-        df.at[i, "Octant"] = 4
-    if(df.at[i, "U'"] >= 0 and df.at[i, "V'"] < 0 and df.at[i, "W'"] < 0):
-        df.at[i, "Octant"] = -4
-
-# df
-
-# mod = int(input("Enter the alue of mod:\n"))
-mod = 5000
-df["Octant ID"] = ''
-df.at[1, "Octant ID"] = "Overall Count"
-df.at[2, "Octant ID"] = f"mod: {mod}"
-
-mn = 5001
-mx = 10000
-
-for i in range(3, 9):
-    if(i == 3):
-        df.at[i, "Octant ID"] = "0000-5000"
-    else:
-        df.at[i, "Octant ID"] = f"{mn} - {mx}"
-        mn = mn + 5000
-        mx = mx + 5000
-
-# df.head(15)
-
-
-for row in range(3, 9):
-    freq = {}
-#     if(row>10):
-#         break
-    if(row == 3):
-        mn = 0
-        mx = mod
-    for i in range(mn, mx):
-        if(i >= 29745):
-            break
-        if(df.at[i, "Octant"] in freq):
-            freq[df.at[i, "Octant"]] = freq[df.at[i, "Octant"]] + 1
-        else:
-            freq[df.at[i, "Octant"]] = 1
-    for key, value in freq.items():
-        df.at[row, key] = value
-        if(df.at[1, key] == ''):
-            df.at[1, key] = 0
-        df.at[1, key] = df.at[1, key] + value
-
-    if(row == 3):
-        mn = mod+1
-        mx = 2*mod
-    if(row > 3):
+    for i in range(3, rowMax + 4):
+        if i is rowMax + 3:
+            mx = sz - 1
+        df.at[i, "Octant ID"] = f"{mn} - {mx}" 
         mn = mn + mod
         mx = mx + mod
 
+    temp = df.at[2, "Octant ID"]
+    # df.head(15)
 
-# for i in range(1, 5000):
-#     if(df.at[i, "Octant"]==1.0):
-#         count1 = count1+1
+    mn = 0
+    mx = mod - 1
 
-# count1
-df = df.fillna('')
-df.head(15)
-# df
+    for row in range(3, rowMax + 4):
+        freq = {}
+        for i in range(mn, mx):
+            if(i >= 29745):
+                break
+            if(df.at[i, "Octant"] in freq):
+                freq[df.at[i, "Octant"]] = freq[df.at[i, "Octant"]] + 1
+            else:
+                freq[df.at[i, "Octant"]] = 1
 
-# def nanToZero(a):
-#     for i in range(sz):
-#         if(df.at[i, a]=="NaN"):
-#             df.at[i, a]=0
-# nanToZero("U Avg")
-# nanToZero("V Avg")
-# nanToZero("W Avg")
+        for key, value in freq.items():
+            df = df.fillna('')
 
-# df["U Avg"].dropna
-# df["V Avg"].dropna
-# df["W Avg"].dropna
-# df.dropna()
+            df.at[row, key] = int(value)
+
+            if(df.at[1, key] == ''):
+                df.at[1, key] = 0
+            df = df.fillna(0)
+            df.at[1, key] = int(df.at[1, key]) + int(value)
+
+        df = df.fillna('')
+
+        mn = mn + mod
+        mx = mx + mod
+
+    print(df.head(30)) # printing top 30 rows to review
+
+    # this part exports df to an output file if not present, otherwise it first deletes it first to avoid overwriting errors i faced in my system 
+    if Path("octant_output.csv").exists():
+        os.remove("octant_output.csv")
+    df.to_csv('octant_output.csv')
+
+    print("mod: ", mod)
+
+mod = 8000
+octant_identification(mod)
