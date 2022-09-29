a1c159d44cfb48e94275205cc77be9346a74d9cb
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-09-29 23:14:14 +0530

    comments added, last commit for tut03 task!

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -48,6 +48,7 @@ def octant_longest_subsequence_count(mod=5000):
     
     # what follows from here onwards is the major part that solves the problem of octant_longest_subsequence_count():
 
+    # initializing required column headers
     df['Count'] = ''
     df["Longest Subsequence Length"] = ''
     df["count"] = ''
@@ -55,12 +56,18 @@ def octant_longest_subsequence_count(mod=5000):
     for i in range(1, 9):
         df.at[i, 'Count'] = int(mp[i])
 
+    # loc dictionary would help telling intergral coordintes required for calculation pruposes
     loc = {1: 0, -1: 1, 2: 2, -2: 3, 3: 4, -3: 5, 4: 6, -4: 7}
+    # initiating "Longest Subsequence Length" and "count" columns with 0s
     for i in range(1, 9):
         df.loc[i, "Longest Subsequence Length"] = int(0)
         df.loc[i, "count"] = int(0)
+    
+    # calculating for longest subsequnce length
+    # i is for octant values, j runs over all rows 
+    # if df.at[j, 'Octant'] matches with i ie the "octant" number, increment, else update 
+    # longest subsequence length and reset the corresponding count stored as temp_ct
     for i in range(-4, 5):
-        print("running at: ", i)
         if i == 0:
             continue
         temp_ct = int(0)
@@ -72,8 +79,10 @@ def octant_longest_subsequence_count(mod=5000):
                     int(temp_ct), df.loc[1 + int(loc[i]), "Longest Subsequence Length"])
                 temp_ct = int(0)
 
+    # calculations for count of total number of "longest subsequence length" 
+    # if temp_ct, that stores count of octant number "i", equals "Longest Subsequence Length"
+    # corresponding count for "i" is incremented 
     for i in range(-4, 5):
-        print("running at: ", i)
         if i == 0:
             continue
         temp_ct = int(0)
@@ -85,9 +94,8 @@ def octant_longest_subsequence_count(mod=5000):
                     df.at[1 + int(loc[i]), "count"] += 1
                 temp_ct = int(0)
 
-    if Path("put_octant_longest_subsequence.xlsx").exists():
-        os.remove("put_octant_longest_subsequence.xlsx")
-    df.to_excel('put_octant_longest_subsequence.xlsx')
-    print(df.head)
+    if Path("output_octant_longest_subsequence.xlsx").exists():
+        os.remove("output_octant_longest_subsequence.xlsx")
+    df.to_excel('output_octant_longest_subsequence.xlsx')
 
 octant_longest_subsequence_count()
