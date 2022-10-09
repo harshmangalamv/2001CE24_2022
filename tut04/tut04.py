98c94959955e2c295e89cf0f0a9d03f8edfea94c
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-10-09 22:47:07 +0530

    all code written minus latest comments

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -96,8 +96,56 @@ def octant_longest_subsequence_count_with_range():
   print("runs3")
   store = [[], [], [], [], [], [], [], []] # stores the last time stamp for longest sequence
 
-octant_longest_subsequence_count_with_range()
+# calculations for count of total number of "longest subsequence length"
+  # if temp_ct, that stores count of octant number "i", equals "Longest Subsequence Length"
+  # corresponding count for "i" is incremented
+  for i in range(-4, 5):
+      print("runs ", i)
+      if i == 0:
+          continue
+      temp_ct = int(0)
+      for j in range(29744):
+          if df.at[j, 'Octant'] == i:
+              temp_ct += 1
+          else:
+              if temp_ct == df.loc[1 + int(loc[i]), "Longest Subsequence Length"]:
+                  df.at[1 + int(loc[i]), "count"] += 1
+                  store[loc[i]].append(df.at[j, "Time"])
+              temp_ct = int(0)
 
+  df['Octant.'] = ''
+  df['Longest Subsequence Length.'] = ''
+  df['Count.'] = ''
+
+  row = 1
+  for i in range(1, 9):
+      # if i is 0:
+      #   continue
+      df.loc[row, "Octant."] = mp[i]
+      print(df.at[loc[mp[i]]+2, "Longest Subsequence Length"])
+      df.loc[row, "Longest Subsequence Length."] = df.at[int(
+          loc[mp[i]])+1, "Longest Subsequence Length"]
+      df.loc[row, "Count."] = df.at[int(loc[mp[i]])+1, "count"]
+      df.loc[row + 1, "Octant."] = "Time"
+      df.loc[row + 1, "Longest Subsequence Length."] = "From"
+      df.loc[row + 1, "Count."] = "To"
+      row += 1
+      for j in range(1, len(store[loc[mp[i]]]) + 1):
+          df.loc[row + j, "Longest Subsequence Length."] = store[loc[mp[i]]][j - 1] - \
+              (int(df.at[int(loc[mp[i]])+1, "Longest Subsequence Length"]) - 1)*0.01
+          df.loc[row + j, "Count."] = store[loc[mp[i]]][j - 1]
+#           row+=1
+      row += (len(store[loc[mp[i]]]) + 1)
+
+  print("runs4")
+
+  if Path("output_octant_longest_subsequence.xlsx").exists():
+    os.remove("output_octant_longest_subsequence.xlsx")
+  df.to_excel('output_octant_longest_subsequence.xlsx')
+  print(df)
+
+octant_longest_subsequence_count_with_range()
+print("well, it runs, really")
 
 # This shall be the last lines of the code.
 end_time = datetime.now()
