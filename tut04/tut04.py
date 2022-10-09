ae3bbd0d03e90dfdb035859de4c3f39d061afc44
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-10-09 22:38:27 +0530

    changed the raw code, added some initial lines

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,29 +1,46 @@
-
+import pandas as pd
+import math
+import os
+from pathlib import Path
 from datetime import datetime
 start_time = datetime.now()
 
+print("runs")
+
 #Help https://youtu.be/H37f_x4wAC0
+
+
 def octant_longest_subsequence_count_with_range():
-###Code
+  ###Code
+  # df
+  # try:
+  df = pd.read_excel(r'input_octant_longest_subsequence_with_range.xlsx')
+  # except:
+  #     print("Error: check if file name is correct, or input file is provided")
+  df['U Avg'] = ''
+  df['V Avg'] = ''
+  df['W Avg'] = ''
+  df.loc[0, 'U Avg'] = df['U'].mean()
+  df.loc[0, 'V Avg'] = df['V'].mean()
+  df.loc[0, 'W Avg'] = df['W'].mean()
+  df["U'"] = ''
+  df["V'"] = ''
+  df["W'"] = ''
+  sz = len(df)
 
-from platform import python_version
-ver = python_version()
+  def fun(a, b, c, sz):
+      for i in range(sz):
+          df.at[i, c] = df.at[i, a] - df.at[0, b]
 
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+  fun("U", "U Avg", "U'", sz)
+  fun("V", "V Avg", "V'", sz)
+  fun("W", "W Avg", "W'", sz)
 
+  print("runs1")
 
 octant_longest_subsequence_count_with_range()
 
 
-
-
-
-
-
-
-#This shall be the last lines of the code.
+# This shall be the last lines of the code.
 end_time = datetime.now()
 print('Duration of Program Execution: {}'.format(end_time - start_time))
