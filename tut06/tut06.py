78e6eff828dcb9e2742035fe1f1e4464fc57fd77
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-11-17 23:19:27 +0530

    another chunk of code

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -28,7 +28,29 @@ def attendance_report():
         '2022-08-25','2022-08-29','2022-09-01', '2022-09-05',
         '2022-09-08','2022-09-12','2022-09-15',
         '2022-09-19','2022-09-22','2022-09-26','2022-09-29']
- 
+
+    count_of_total_classes_official = 18
+    totalday_count=len(lectureday)
+    df = pd.DataFrame(columns=['Roll','Name'] + lectureday)
+    df.at[0,'Actual Lecture Taken']='Total monday+ thurs dynamic count'
+    
+    for i in range(221):
+        temp_list = []
+        roll = df_reg_stud['Roll No'][i]
+        name = df_reg_stud['Name'][i]
+        df_1 = pd.DataFrame(columns=['Dates'])
+        k = 0
+        for j in range(0, len(df_input_attend['Timestamp'])):
+            a1 = type(df_input_attend.at[j, 'Attendance'])
+            if(a1 == float):
+                continue  # to skip some errors I faced, this fixed, idk how
+            if (df_input_attend.at[j, 'Attendance'].split()[0] == roll):
+                df_1.at[k, 'Dates'] = df_input_attend.at[j, 'Timestamp']
+                k = k + 1
+        df_1['Dates'] = pd.to_datetime(df_1['Dates'], dayfirst=True)
+
+        
+
 
 attendance_report()
 
