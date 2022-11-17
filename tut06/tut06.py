d67f9b2a726db379210e46ce5987558e01149146
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-11-17 22:53:50 +0530

    tut06 initial changes  done

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,19 +1,34 @@
+# importing different requirements
+import pandas as pd
+from numpy import nan
+import numpy as np
+import os
+import calendar
+import math
+import datetime
+start_time = datetime.datetime.now()
 
+pd.options.mode.chained_assignment = None  # default='warn'
 
-from datetime import datetime
-start_time = datetime.now()
+#function to calcualate the day of the week when we give the input in format of string as dd/mm/yy
+
+def findDay(date):
+    day, month, year = (int(i) for i in date.split('/'))
+    born = datetime.date(year, month, day)
+    return born.strftime("%A")
 
 def attendance_report():
 ###Code
+    df_reg_stud = pd.read_csv('tut06\input_registered_students.csv')
+    df_input_attend = pd.read_csv('tut06\input_attendance.csv')
 
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
+    # (before running for updated attendance list, update this manunally) to store dates of lecture in yy-mm-dd
+    lectureday = ['2022-07-28', '2022-08-01', '2022-08-04', '2022-08-08',
+        '2022-08-12', '2022-08-15', '2022-08-18', '2022-08-22',
+        '2022-08-25','2022-08-29','2022-09-01', '2022-09-05',
+        '2022-09-08','2022-09-12','2022-09-15',
+        '2022-09-19','2022-09-22','2022-09-26','2022-09-29']
+ 
 
 attendance_report()
 
