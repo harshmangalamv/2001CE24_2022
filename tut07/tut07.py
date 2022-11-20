d4e8ff59cf347c1b8d4e756fb0eaacd0b69e532d
Harsh Mangalam Verma <harshmangalamv@gmail.com>
2022-11-20 22:26:35 +0530

    tut_07 gets its first commit
    edited and wrote initial lines

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -1,32 +1,28 @@
+# Writer:
+# Harsh Mangalam Verma (IIT Patna) 
+# For tut07
+import pandas as pd
+import math
+import os
+from pathlib import Path
+input_dir = "input"
 
 from datetime import datetime
 start_time = datetime.now()
 
 #Help
-def octant_analysis(mod=5000):
-	pass
-
-##Read all the excel files in a batch format from the input/ folder. Only xlsx to be allowed
-##Save all the excel files in a the output/ folder. Only xlsx to be allowed
-## output filename = input_filename[_octant_analysis_mod_5000].xlsx , ie, append _octant_analysis_mod_5000 to the original filename. 
-
-###Code
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-
-mod=5000
-octant_analysis(mod)
-
-
-
-
+def octant_identification(mod = 4000):
+    for file in os.listdir(input_dir):
+        f = os.path.join(input_dir, file)
+        file_name = file[:-5]
+        print(file_name)
+#         continue
+        df = pd.read_excel(f)
+            # creating and populating colums that store Avg values
+        df.at[0, "U Avg"] = df["U"].mean()
+        df.at[0, "V Avg"] = df["V"].mean()
+        df.at[0, "W Avg"] = df["W"].mean()
+        sz = len(df["T"])
 
 
 #This shall be the last lines of the code.
